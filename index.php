406596edff03add5793e55f2f15ba4e694533749
rreeggeenntt4 <114906878+rreeggeenntt4@users.noreply.github.com>
2023-03-17 20:26:20 +0300

    Стартовое задание

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+function normalize($input)
+{
+    /* return "<not implemented yet>"; */
+}
+
+$input1 = "/var/./lib/../test";
+$input2 = "/var/lib/../../test";
+$input3 = "/var/lib1/lib2/lib3/././././././lib4/../../../../lib5";
+
+$output1 = normalize($input1);
+$output2 = normalize($input2);
+$output3 = normalize($input3);
+
+
+echo $output1 . "\n"; // expected output: /var/test
+echo $output2 . "\n"; // expected output: /test
+echo $output3 . "\n"; // expected output: /var/lib5 
+
